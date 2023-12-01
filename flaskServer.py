a7621d0b4ba92bb9109035c8c7d7f65c4b37cc65
nitzanmr <81086233+nitzanmr@users.noreply.github.com>
2023-12-01 03:13:11 +0200

    changed flusk to work with js with tweets.txt

--- flaskServer.py
+++ flaskServer.py
@@ -1,23 +1,26 @@
 from flask import Flask, request, jsonify, render_template
-from postgres_insert import connect_db, get_data_from_db, add_data
+# from postgres_insert import connect_db, get_data_from_db, add_data
 from datetime import datetime
 from flask_socketio import SocketIO
+from flask_cors import CORS
+import json
 
-
-conn = connect_db() # TODO check if it is setting correctly
-app = Flask(__name__)
+# conn = connect_db() # TODO check if it is setting correctly
+app = Flask(_name_)
+CORS(app, resources={r"*": {"origins": "*"}})
 app.config['SECRET_KEY'] = 'posthunter1!'
 socketio = SocketIO(app)
 
 
-@sock.route('/')
-def handle_connect():
-    print('New connection established')
+# @sock.route('/')
+# def handle_connect():
+#     print('New connection established')
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return 'SUCCESS TO CONNECT TO BACKEND SERVER'
+    #return render_template('index.html')
 
 
 # tweet_id = int 
@@ -26,25 +29,40 @@ def index():
 # status_link = url standart 
 # analytics_link = ###
 
-@app.route('/tweets', methods=['POST'])
-def write_tweet_to_file(tweet):
+def read_tweets_from_file():
     try:
+        with open('tweets.txt', 'r') as file:
+            tweets = json.load(file)
+        return tweets
+    except FileNotFoundError:
+        return []
+
+@app.route('/storeTweet', methods=['POST'])
+def write_tweet_to_file():
+    try:
+        tweet = request.json
+        tweets = read_tweets_from_file()
+        tweets.append(tweet)
         with open('tweets.txt', 'w') as file:
-            file.write(tweet)
-        print('File written successfully.')
+            file.write(json.dumps(tweets))  # Convert the JSON object to a string
+        print('Tweet written to file successfully.')
+        return json.dumps(tweet)  # Return the JSON object as a response
     except Exception as e:
         print('Error writing to file:', str(e))
-
-@app.route('/tweets/<int:tweet_id>', methods=['GET'])    
-def read_tweet_to_file(tweet_id):
+        return json.dumps({'error': str(e)}), 500  # Return an error response with 500 status code
+    
+@app.route('/getTweets', methods=['GET'])    
+def read_tweet_to_file():
     try:
         with open('tweets.txt', 'r') as file:
             content = file.read()
+            print('content from file: ', content)
             return content
-        print('File written successfully.')
     except Exception as e:
         print('Error writing to file:', str(e))
-
+        return e
+    
+    
 # @app.route('/tweets', methods=['POST'])
 # def add_tweet():
 #     try:
@@ -65,7 +83,6 @@ def read_tweet_to_file(tweet_id):
 #     return data
 
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(port=81)
     socketio.run(app)
-    
